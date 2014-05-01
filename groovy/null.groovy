c36e425dec69e4f89e56128e352134352ff33c76
Simon Olofsson <simon@olofsson.de>
2014-05-01 12:56:26 +0200

    Add some Comments.

--- groovy/null.groovy
+++ groovy/null.groovy
@@ -1,8 +1,11 @@
 def conference = [name: "JavaLand"]
 
+// ternärer Operator
 println conference.name ?
 	conference.name : "Unknown"
 
+// Elvis-Operator
 println conference.name ?: "Unknown"
 
+// Safe-Navigation-Operator
 conference.name?.toLowerCase()
